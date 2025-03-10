18ca808544080ebbf59858b87d8616bbec472086
ronald azevedo <senac.ronaldazevedo@gmail.com>
2025-03-10 19:25:24 -0300

    Atualiza com a nova versao .

---
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MultApps.Windows
+{
+    partial class FrmCalculadoraIMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chkFeminino = new System.Windows.Forms.CheckBox();
+            this.chkMasculino = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkCrianca = new System.Windows.Forms.CheckBox();
+            this.chkAdulto = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblIdade);
+            this.panel1.Controls.Add(this.chkFeminino);
+            this.panel1.Controls.Add(this.chkMasculino);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.chkCrianca);
+            this.panel1.Controls.Add(this.chkAdulto);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 28);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1549, 759);
+            this.panel1.TabIndex = 0;
+            // 
+            // chkFeminino
+            // 
+            this.chkFeminino.AutoSize = true;
+            this.chkFeminino.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFeminino.ForeColor = System.Drawing.Color.Gray;
+            this.chkFeminino.Location = new System.Drawing.Point(743, 82);
+            this.chkFeminino.Name = "chkFeminino";
+            this.chkFeminino.Size = new System.Drawing.Size(176, 41);
+            this.chkFeminino.TabIndex = 5;
+            this.chkFeminino.Text = "Feminino";
+            this.chkFeminino.UseVisualStyleBackColor = true;
+            // 
+            // chkMasculino
+            // 
+            this.chkMasculino.AutoSize = true;
+            this.chkMasculino.Checked = true;
+            this.chkMasculino.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMasculino.ForeColor = System.Drawing.Color.Orange;
+            this.chkMasculino.Location = new System.Drawing.Point(537, 82);
+            this.chkMasculino.Name = "chkMasculino";
+            this.chkMasculino.Size = new System.Drawing.Size(189, 41);
+            this.chkMasculino.TabIndex = 4;
+            this.chkMasculino.Text = "Masculino";
+            this.chkMasculino.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(532, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sexo";
+            // 
+            // chkCrianca
+            // 
+            this.chkCrianca.AutoSize = true;
+            this.chkCrianca.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCrianca.ForeColor = System.Drawing.Color.Gray;
+            this.chkCrianca.Location = new System.Drawing.Point(212, 82);
+            this.chkCrianca.Name = "chkCrianca";
+            this.chkCrianca.Size = new System.Drawing.Size(154, 41);
+            this.chkCrianca.TabIndex = 2;
+            this.chkCrianca.Text = "Criança";
+            this.chkCrianca.UseVisualStyleBackColor = true;
+            this.chkCrianca.Click += new System.EventHandler(this.chkCrianca_CheckedChanged);
+            // 
+            // chkAdulto
+            // 
+            this.chkAdulto.AutoSize = true;
+            this.chkAdulto.Checked = true;
+            this.chkAdulto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAdulto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAdulto.ForeColor = System.Drawing.Color.Orange;
+            this.chkAdulto.Location = new System.Drawing.Point(78, 82);
+            this.chkAdulto.Name = "chkAdulto";
+            this.chkAdulto.Size = new System.Drawing.Size(134, 41);
+            this.chkAdulto.TabIndex = 1;
+            this.chkAdulto.Text = "Adulto";
+            this.chkAdulto.UseVisualStyleBackColor = true;
+            this.chkAdulto.Click += new System.EventHandler(this.chkAdulto_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(73, 143);
+            this.lblIdade.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(184, 25);
+            this.lblIdade.TabIndex = 6;
+            this.lblIdade.Text = "Acima de 19 anos";
+            // 
+            // FrmCalculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(1599, 831);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FrmCalculadoraIMC";
+            this.Text = "FrmCalculadoraIMC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkCrianca;
+        private System.Windows.Forms.CheckBox chkAdulto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkFeminino;
+        private System.Windows.Forms.CheckBox chkMasculino;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblIdade;
+    }
+}
---
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class FrmCalculadoraIMC : Form
+    {
+        public FrmCalculadoraIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void chkCrianca_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCrianca.ForeColor = Color.DarkOrange;
+            chkAdulto.ForeColor = Color.Gray;
+            chkAdulto.Checked = false;
+            lblIdade.Text = "Abaixo de 19 anos";
+        }
+
+        private void chkAdulto_CheckedChanged(object sender, EventArgs e)
+        {
+            chkAdulto.ForeColor = Color.DarkOrange;
+            chkCrianca.ForeColor = Color.Gray;
+            chkCrianca.Checked = false;
+            lblIdade.Text = "Acima de 19 anos";
+        }
+    }
+}
