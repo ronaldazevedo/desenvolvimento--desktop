8c01945c77bf70724e104d8a1814c2bc7159c38f
jessica0027 <jessicacorrea0027@gmail.com>
2025-04-14 21:56:36 -0300

    feat: estou fazendo o codigo tem alguns erros

---
+++ MultApps/VIEW/MultApps.Windows/FrmCadastroUsuarios.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace MultApps.Windows
+{
+    partial class FrmCadastroUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblCategoriaProdutos = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtDataAlteracao = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblDataAlteracao = new System.Windows.Forms.Label();
+            this.txtDataCadastro = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblDataCadastro = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(345, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(437, 465);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // lblCategoriaProdutos
+            // 
+            this.lblCategoriaProdutos.AutoSize = true;
+            this.lblCategoriaProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoriaProdutos.Location = new System.Drawing.Point(28, 19);
+            this.lblCategoriaProdutos.Name = "lblCategoriaProdutos";
+            this.lblCategoriaProdutos.Size = new System.Drawing.Size(197, 16);
+            this.lblCategoriaProdutos.TabIndex = 19;
+            this.lblCategoriaProdutos.Text = "CADASTRO DE USUÁRIOS";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.lblSenha);
+            this.groupBox1.Controls.Add(this.txtSenha);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.lblCpf);
+            this.groupBox1.Controls.Add(this.txtCpf);
+            this.groupBox1.Controls.Add(this.btnDeletar);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Controls.Add(this.btnSalvar);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Controls.Add(this.cmbStatus);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.txtDataAlteracao);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.lblDataAlteracao);
+            this.groupBox1.Controls.Add(this.txtDataCadastro);
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Controls.Add(this.lblDataCadastro);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(770, 465);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CADASTRAR NOVO USUÁRIO";
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Enabled = false;
+            this.btnDeletar.Location = new System.Drawing.Point(259, 415);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(68, 23);
+            this.btnDeletar.TabIndex = 17;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(168, 415);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(63, 23);
+            this.btnLimpar.TabIndex = 16;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(6, 43);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(20, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnSalvar.Location = new System.Drawing.Point(9, 415);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(124, 23);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(41, 43);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(48, 16);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Ativo ",
+            "Inativo ",
+            "Todos"});
+            this.cmbStatus.Location = new System.Drawing.Point(6, 332);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(103, 23);
+            this.cmbStatus.TabIndex = 13;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(6, 61);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(32, 21);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtDataAlteracao
+            // 
+            this.txtDataAlteracao.Enabled = false;
+            this.txtDataAlteracao.Location = new System.Drawing.Point(139, 269);
+            this.txtDataAlteracao.Name = "txtDataAlteracao";
+            this.txtDataAlteracao.Size = new System.Drawing.Size(155, 21);
+            this.txtDataAlteracao.TabIndex = 12;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(44, 62);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(250, 21);
+            this.txtNome.TabIndex = 3;
+            // 
+            // lblDataAlteracao
+            // 
+            this.lblDataAlteracao.AutoSize = true;
+            this.lblDataAlteracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataAlteracao.Location = new System.Drawing.Point(136, 250);
+            this.lblDataAlteracao.Name = "lblDataAlteracao";
+            this.lblDataAlteracao.Size = new System.Drawing.Size(162, 16);
+            this.lblDataAlteracao.TabIndex = 10;
+            this.lblDataAlteracao.Text = "Data de ultimo acesso";
+            // 
+            // txtDataCadastro
+            // 
+            this.txtDataCadastro.Enabled = false;
+            this.txtDataCadastro.Location = new System.Drawing.Point(6, 269);
+            this.txtDataCadastro.Name = "txtDataCadastro";
+            this.txtDataCadastro.Size = new System.Drawing.Size(127, 21);
+            this.txtDataCadastro.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(7, 313);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(50, 16);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblDataCadastro
+            // 
+            this.lblDataCadastro.AutoSize = true;
+            this.lblDataCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataCadastro.Location = new System.Drawing.Point(6, 250);
+            this.lblDataCadastro.Name = "lblDataCadastro";
+            this.lblDataCadastro.Size = new System.Drawing.Size(120, 16);
+            this.lblDataCadastro.TabIndex = 7;
+            this.lblDataCadastro.Text = "Data de Criação";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(6, 110);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(288, 21);
+            this.txtCpf.TabIndex = 18;
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpf.Location = new System.Drawing.Point(6, 91);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(36, 16);
+            this.lblCpf.TabIndex = 19;
+            this.lblCpf.Text = "CPF";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(9, 162);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(285, 21);
+            this.txtEmail.TabIndex = 20;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(6, 143);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 16);
+            this.lblEmail.TabIndex = 21;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(6, 215);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(288, 21);
+            this.txtSenha.TabIndex = 22;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(6, 196);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(51, 16);
+            this.lblSenha.TabIndex = 23;
+            this.lblSenha.Text = "Senha";
+            // 
+            // FrmCadastroUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 527);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblCategoriaProdutos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmCadastroUsuarios";
+            this.Text = "FrmCadastroUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCategoriaProdutos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtDataAlteracao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblDataAlteracao;
+        private System.Windows.Forms.TextBox txtDataCadastro;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblDataCadastro;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+    }
+}
---
+++ MultApps/VIEW/MultApps.Windows/FrmCadastroUsuarios.cs
@@ -0,0 +1,146 @@
+﻿using MultApps.Models.Enums;
+using MultApps.Models.Repositories;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MultApps.Windows
+{
+    public partial class FrmCadastroUsuarios : Form
+    {
+        public FrmCadastroUsuarios()
+        {
+            InitializeComponent();
+
+        }
+        private void CarregarTodasCategorias()
+        {
+            var categoriaRepository = new CategoriaRepository();
+            var listaDeCategorias = categoriaRepository.ListarTodasCategorias();
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Clear();
+
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Id",
+                HeaderText = "Id",
+                MinimumWidth = 100,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Nome",
+                HeaderText = "Nome de usuário",
+                MinimumWidth = 300,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Cpf",
+                HeaderText = "CPF",
+                MinimumWidth = 300,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Email",
+                HeaderText = "Email",
+                MinimumWidth = 300,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Senha",
+                HeaderText = "Senha",
+                MinimumWidth = 300,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "DataCriação",
+                HeaderText = "Data de Criação",
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "dd/MM/yyyy HH:MM" },
+                MinimumWidth = 200
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "DataAlteracao",
+                HeaderText = "Data de Ultimo Acesso",
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "dd/MM/yyyy HH:MM" },
+                MinimumWidth = 200
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Status",
+                HeaderText = "Status",
+                DefaultCellStyle = new DataGridViewCellStyle
+                {
+
+                }
+            });
+
+            dataGridView1.DataSource = listaDeCategorias;
+            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+
+        }
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].DataPropertyName == "Status")
+            {
+                if (e.Value != null)
+                {
+                    StatusFiltro Status = (StatusFiltro)e.Value;
+                    switch (Status)
+                    {
+                        case StatusFiltro.Inativo:
+                            e.CellStyle.ForeColor = Color.Gray;
+                            break;
+                        case StatusFiltro.Ativo:
+                            e.CellStyle.ForeColor = Color.Blue;
+                            break;
+                        case StatusFiltro.Todos:
+                            e.CellStyle.ForeColor = Color.Red;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                MessageBox.Show($"Houve um erro ao clicar duas vezes sobre o Grid");
+                return;
+            }
+
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+            var cadastroId = (int)row.Cells[0].Value;
+
+            var categoriaRepository = new CategoriaRepository();
+            var categoria = categoriaRepository.ObterCategoriaPorId(cadastroId);
+
+            if (categoria == null)
+            {
+                MessageBox.Show($"Categoria: #{cadastroId} não encontrada");
+                return;
+            }
+           
+            txtId.Text = categoria.Id.ToString();
+            txtNome.Text = categoria.Nome;
+            txtCpf.Text = categoria.CPF;
+            txtEmail.Text = categoria.mail;
+            cmbStatus.SelectedIndex = (int)categoria.Status;
+            txtDataCadastro.Text = categoria.DataCriacao.ToString("dd/MM/yyyy HH:mm");
+            txtDataAlteracao.Text = categoria.DataAlteracao.ToString("dd/MM/yyyy HH:mm");
+
+            btnDeletar.Enabled = true;
+            btnSalvar.Text = "Salvar alterações";
+        }
+    }
+}
